d26dbe7cae734124bb73322c4df490b94eba8af1
= <=>
2021-07-28 20:37:04 +0700

    add demo authorization

--- src/main/java/com/example/crud/config/WebSecurityConfig.java
+++ src/main/java/com/example/crud/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.crud.security;
+package com.example.crud.config;
 
 import com.example.crud.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,15 +9,9 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import javax.annotation.Resource;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -37,9 +31,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder(12);
     }
 
+    //Authentication config
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authProvider());
+    }
+
+    //Authorization config
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").authenticated()//Login required to access home page
+                .anyRequest().hasAuthority("admin")//Admin role required to create, update, delete DB
+                .and()
+                .formLogin()
+               // .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+
     }
 
 }
